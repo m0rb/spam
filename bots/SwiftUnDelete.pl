dabd17091d5e861168f87a6eacd901553438d149
m0rb <chris@cataclysmal.org>
2016-03-29 09:25:25 -0700

    Now with chunked media upload support... both images and videos will now post to twitter.

--- bots/SwiftUnDelete.pl
+++ bots/SwiftUnDelete.pl
@@ -27,6 +27,7 @@ my $at   = $settings->val( 'twitter', 'at' )   or die;
 my $asec = $settings->val( 'twitter', 'asec' ) or die;
 ################################################################
 my $tweets = "swift.tweets";
+my $imgdir = "/tmp";
 my @swifttweets;
 eval {
     @swifttweets = @{ retrieve($tweets) };
@@ -58,49 +59,88 @@ my $stream = AnyEvent::Twitter::Stream->new(
         my $msg   = $t->{text} or return 1;
         my $taco  = $t->{entities}->{media}->[0]->{url};
         my $media = $t->{entities}->{media}->[0]->{media_url};
+        my $vid =
+          $t->{extended_entities}{media}[0]{video_info}{variants}[0]{url};
         if ( $sn eq "SwiftOnSecurity" ) {
             print "$tid: <$sn> $msg\n";
             if ($taco) { $msg =~ s/$taco//g; }
             push @swifttweets, ( $tid . $msg )
               and store( \@swifttweets, $tweets );
-            if ($media) {
-                my $ua   = LWP::UserAgent->new;
-                my $req  = HTTP::Request->new( GET => $media );
-                my $resp = $ua->request($req);
-                if ( $resp->is_success ) {
-                    open( IMG, ">/tmp/${tid}.jpg" ) or die "$!";
-                    binmode(IMG);
-                    print IMG $ua->request($req)->content;
-                    close(IMG);
-                }
+            if ($vid) {
+                print "fetching video\n";
+                &fetch( $vid, "${imgdir}/${tid}.mp4" );
             }
+            elsif ($media) {
+                print "fetching jpeg\n";
+                &fetch( $media, "${imgdir}/${tid}.jpg" );
+            }
+
         }
     },
     on_delete => sub {
-        my ( $tid, $uid ) = @_;
-        my $file = "/tmp/${tid}.jpg";
+        my ( $tid, $uid ) = @_; my $mid = 0;
+        my $fn = "${imgdir}/${tid}";
         my @swiftsort = grep { /$tid/ } @swifttweets;
         if (@swiftsort) {
             my ($output) = @swiftsort;
             $output =~ s/$tid//g;
             print colored( "<TayBot> $output\n", 'red' );
-            if ( -e $file ) {
-                open( IMG, $file );
-                my $rawdog = do { local $/ = undef; <IMG>; };
-                my $enc = encode_base64($rawdog);
-                close(IMG);
-                my $upload = $nt->upload( { media => $enc } )
-                  or die "$@\n"; 
-                eval {
-                    $nt->update(
-                        { status => $output, media_ids => $upload->{media_id} }
-                    );
-                };
+            if ( -e "${fn}.mp4" ) {
+                $mid = &chunklet( "video/mp4", "${fn}.mp4" );
             }
-            else {
-                eval { $nt->update( { status => "$output" } ); };
+            elsif ( -e "${fn}.jpg" ) {
+                $mid = &chunklet( "image/jpeg", "${fn}.jpg" );
             }
+            eval { $nt->update( { status => $output, media_ids => $mid } ); };
         }
     },
 );
 $derp->recv;
+
+sub fetch {
+    my ( $media, $filename ) = @_;
+    my $ua   = LWP::UserAgent->new;
+    my $req  = HTTP::Request->new( GET => $media );
+    my $resp = $ua->request($req);
+    if ( $resp->is_success ) {
+        open F, ">${filename}" or die "$!";
+        binmode F;
+        print F $ua->request($req)->content;
+        close F;
+    }
+}
+
+sub chunklet {
+    die unless $nt->authorized;
+    my ( $mt, $fn ) = @_;
+    my $fs = -s $fn;
+    my $si = 0;
+    my $init = $nt->upload(
+        { command => 'INIT', media_type => $mt, total_bytes => $fs } )
+      or die $@;
+    my $media_id = $init->{media_id};
+    open( IMAGE, $fn ) or die "!:$!\n";
+    binmode IMAGE;
+
+    while ( read( IMAGE, my $chunk, 1048576 ) ) {
+        my $file = [
+            undef, 'media',
+            Content_Type => 'form-data',
+            Content      => encode_base64($chunk)
+        ];
+        eval {
+            $nt->upload(
+                {
+                    command       => 'APPEND',
+                    media_id      => $media_id,
+                    segment_index => $si,
+                    media_data    => $file
+                }
+            );
+        };
+        $si += 1;
+    }
+    $nt->upload( { command => 'FINALIZE', media_id => $media_id } );
+    close(IMAGE);
+    return ($media_id);
+}
